ea885b316c5c24abf109548d9e87d87d305681b2
Andrew Kane <andrew@ankane.org>
2026-08-19 20:31:54 -0700

    Updated style [skip ci]

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -94,7 +94,13 @@ func TestElasticsearch(t *testing.T) {
 	}
 
 	ctx := context.TODO()
-	_, err = es.Indices.Delete(ctx, esapi.IndicesDeleteReq{Indices: []string{"pdscan_test_users"}, Params: esapi.IndicesDeleteParams{IgnoreUnavailable: esapi.ToPointer(true)}})
+	_, err = es.Indices.Delete(
+		ctx,
+		esapi.IndicesDeleteReq{
+			Indices: []string{"pdscan_test_users"},
+			Params:  esapi.IndicesDeleteParams{IgnoreUnavailable: esapi.ToPointer(true)},
+		},
+	)
 	if err != nil {
 		panic(err)
 	}
